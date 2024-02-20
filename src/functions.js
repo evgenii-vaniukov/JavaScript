77e86d9579b44ed5e07777dc3e49dfbeb6f8c24c
evgenii-vaniukov <evgeniivaniukov64@gmail.com>
2024-02-20 10:56:54 +0200

    memoize

--- src/functions.js
+++ src/functions.js
@@ -28,3 +28,36 @@ var once = function (fn) {
     }
   };
 };
+
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+  const cache = new Map();
+
+  return function (...args) {
+    const key = JSON.stringify(args);
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
+    const result = fn(...args);
+    cache.set(key, result);
+    return result;
+  };
+}
+
+// Create a dict with keys sum, fib, factorial, and values as sets of arguments (as strings).
+// When a function is called, arguments go into a designated set as strings.
+// Before every call, cache is checked.
+
+/**
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1
+ */
